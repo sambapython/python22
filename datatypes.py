9a2dcd1950b7dd7391c0fba460e325f234666edc
sambapython@gmail.com <sambapython@gmail.com>
2016-11-24 08:50:14 +0530

    2016-NOV-23

---
+++ datatypes.py
@@ -0,0 +1,674 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# int
+a=100
+b=200
+print a+b
+print a-b
+print a*b
+print a/b
+print a**2
+print a%5
+
+
+# In[2]:
+
+print 10/100
+
+
+# In[3]:
+
+print 100/5
+
+
+# In[4]:
+
+print 100.0/5
+
+
+# In[5]:
+
+print 100.0/200
+
+
+# In[7]:
+
+print a
+print b
+
+
+# In[8]:
+
+print a.0
+
+
+# In[9]:
+
+a=float(a)
+
+
+# In[10]:
+
+a
+
+
+# In[11]:
+
+print a/b
+
+
+# In[12]:
+
+a=10
+b=20
+print float(a/b)
+
+
+# In[13]:
+
+float(0)
+
+
+# In[14]:
+
+a=10
+b=20
+print float(a)/b
+
+
+# In[15]:
+
+a=10
+b=20
+print float(a)/float(b)
+
+
+# In[16]:
+
+print a/float(b)
+
+
+# In[17]:
+
+s="str1"
+
+
+# In[18]:
+
+s1="str1"
+s2='str2'
+print s1+s2
+
+
+# In[19]:
+
+print s1-s2
+
+
+# In[20]:
+
+print s1*s2
+
+
+# In[21]:
+
+print s1/s2
+
+
+# In[22]:
+
+print "str1"+10
+
+
+# In[23]:
+
+print "str1"*2
+
+
+# In[24]:
+
+print "str1"*8
+
+
+# In[25]:
+
+#"********************************************************"
+print "*"*100
+
+
+# In[26]:
+
+print "str1"/2
+
+
+# In[27]:
+
+a=10
+b=20
+print a+b
+
+
+# In[28]:
+
+a=raw_input("Enter a value:")
+b= raw_input("Enter b value:")
+print a+b
+
+
+# In[30]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+print a+b
+
+
+# In[31]:
+
+a="10"
+b="20"
+print a+b
+
+
+# In[32]:
+
+print int(a)+int(b)
+
+
+# In[33]:
+
+print int(a)
+
+
+# In[ ]:
+
+a="10"
+b="20"
+c=int(a+b)
+d=int(a)+int(b)
+print c, type(c)
+print d, type(d)
+
+
+# In[34]:
+
+a=10
+print a
+print "a"
+print "hello world"
+
+
+# In[35]:
+
+print 20
+
+
+# In[36]:
+
+print z
+print "z"
+
+
+# In[37]:
+
+print "z"
+
+
+# In[38]:
+
+print z
+
+
+# In[39]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+print type(a)
+print type(b)
+
+
+# In[40]:
+
+a="10"
+b="23"
+print a+b
+
+
+# In[41]:
+
+a1=int(a)
+b1=int(b)
+print type(a1),a1
+
+
+# In[42]:
+
+print a1+b1
+
+
+# In[43]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=int(a)
+b1=int(b)
+print a1+b1
+
+
+# In[44]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=int(a)
+b1=int(b)
+print a1+b1
+
+
+# In[45]:
+
+print int(12.34)
+
+
+# In[46]:
+
+print int("34.56")
+
+
+# In[47]:
+
+int("12.34")
+
+
+# In[48]:
+
+int(12.34)
+
+
+# In[50]:
+
+a=12.34
+b="12.34"
+print a,type(a)
+print b, type(b)
+
+
+# In[51]:
+
+a="10"
+b="10.0"
+print int(a)
+print int(b)
+
+
+# In[52]:
+
+# base 10: 01234546789 -> . # a-zint
+
+print int("10")
+print int("10a")
+
+
+# In[53]:
+
+# base 10
+get_ipython().magic(u'pinfo int')
+
+
+# In[54]:
+
+print int()
+
+
+# In[55]:
+
+a=raw_input("Enter a data:")
+print int(a)
+
+
+# In[56]:
+
+a=raw_input("Enter a data:")
+print int(a)
+
+
+# In[57]:
+
+a=raw_input("Enter a data:")
+print int(a)
+
+
+# In[58]:
+
+a=raw_input("Enter a data:")
+print a
+
+
+# In[59]:
+
+print type(a)
+
+
+# In[60]:
+
+b=''
+
+
+# In[61]:
+
+print b
+
+
+# In[62]:
+
+a=None
+
+
+# In[64]:
+
+print a
+
+
+# In[65]:
+
+print int('10a')
+
+
+# In[66]:
+
+print int('10a',base=11)
+
+
+# In[67]:
+
+print 10*11**0+0*11**1+1*11**2
+
+
+# In[68]:
+
+print int('10abc',base=11)
+
+
+# In[69]:
+
+print int('10abc',base=13)
+
+
+# In[70]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=int(a)
+b1=int(b)
+print a1+b1
+
+
+# In[71]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=int(a)
+b1=int(b)
+print a1+b1
+
+
+# In[72]:
+
+a=12.34
+b=4
+
+
+# In[73]:
+
+print a+b
+print a-b
+print a/b
+print a*b
+print a**2
+print a%2
+
+
+# In[74]:
+
+a="2.34"
+print float(a)
+
+
+# In[75]:
+
+print int(a)
+
+
+# In[76]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=float(a)
+b1=float(b)
+print a1+b1
+
+
+# In[77]:
+
+a=raw_input("Enter a value(only number):")
+b=raw_input("Enter b value(only number):")
+a1=float(a)
+b1=float(b)
+print a1+b1
+
+
+# In[78]:
+
+c1=2+3j
+c2=3+4j
+print c1+c2
+print c1-c2
+print c1*c2
+print c1/c2
+
+
+# In[79]:
+
+emp_name = "Anil"
+salary_total = 1234
+
+
+# In[80]:
+
+asWER = "sfsdf"
+
+
+# In[81]:
+
+12wer = "sdfsd"
+
+
+# In[82]:
+
+a123 = "sdfsdf"
+
+
+# In[83]:
+
+awer@#sf = "sdfsdf"
+
+
+# In[84]:
+
+asd_ert = "sdfsdfds"
+
+
+# In[85]:
+
+# boolean operations
+
+
+# In[86]:
+
+10>20
+
+
+# In[87]:
+
+emp_name = "Anil"
+print emp_name
+
+
+# In[88]:
+
+print False
+
+
+# In[89]:
+
+print True
+
+
+# In[90]:
+
+print None
+
+
+# In[91]:
+
+a=False
+
+
+# In[92]:
+
+b=False
+
+
+# In[93]:
+
+c=None
+
+
+# In[94]:
+
+d=True
+
+
+# In[95]:
+
+print type(False)
+
+
+# In[96]:
+
+print type(True)
+
+
+# In[97]:
+
+print true
+
+
+# In[98]:
+
+10>20
+
+
+# In[99]:
+
+20<30
+
+
+# In[100]:
+
+100==100
+
+
+# In[101]:
+
+100==200
+
+
+# In[102]:
+
+a=10
+
+
+# In[103]:
+
+a==100
+
+
+# In[107]:
+
+a=10
+b=10.0
+print a==b
+print a is b
+print id(a)
+print id(b)
+
+
+# In[108]:
+
+"python" in "python program"
+
+
+# In[109]:
+
+"java" in "python program"
+
+
+# In[110]:
+
+a=12
+b=012
+print a
+print b
+
+
+# In[112]:
+
+a=0x12
+print a
+print 2*16**0+1*16**1
+
+
+# In[113]:
+
+a=0b010101
+print a
+
+
+# In[114]:
+
+a=0o12
+b=012
+print a
+print b
+
+
+# In[115]:
+
+a=0x12abc
+print a
+
+
+# In[116]:
+
+b=0b0123
+print b
+
+
+# In[117]:
+
+c=0o127
+print c
+
+
+# In[118]:
+
+c=0o128
+print c
+
+
+# In[ ]:
+
+
+
